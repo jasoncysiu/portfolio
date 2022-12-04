db7ea05c656f1246bf5f66a38e6278e634911621
algaespace@gmail.com <sjsa3a28@gmail.com>
2022-12-04 20:54:41 +1100

    Update SocialButton.js

--- themes/hexo/components/SocialButton.js
+++ themes/hexo/components/SocialButton.js
@@ -27,7 +27,7 @@ const SocialButton = () => {
       {BLOG.CONTACT_EMAIL && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${BLOG.CONTACT_EMAIL}`} >
         <i className='transform hover:scale-125 duration-150 fas fa-envelope dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>}
-      <a target='_self' rel='noreferrer' title={'Quotes'} href={'/quotes'} >
+      <a target='_self' rel='noreferrer' title={'Quotes'} href={'https://www.jason-siu.com/quotes'} >
         <i className='transform hover:scale-125 duration-150 fas fa-quote-left dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>
       <a target='_self' rel='noreferrer' title={'RSS'} href={'/feed'} >
