416f665d1073d154d0eb071297870ed92454edcd
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-10-07 14:07:37 +1100

    fe: Update typeface

--- blog.config.js
+++ blog.config.js
@@ -28,9 +28,16 @@ const BLOG = {
 
   // 自定义字体示例： 请先将 CUSTOM_FONT 改为 true， 并将 CUSTOM_FONT_URL 改为你的字体CSS地址，同时在 CUSTOM_FONT_SANS 与 CUSTOM_FONT_SERIF 中指定你的 fontfamily
   CUSTOM_FONT: true, // 是否使用自定义字体
-  CUSTOM_FONT_URL: ['https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css'], // 自定义字体的CSS
-  CUSTOM_FONT_SANS: ['LXGW WenKai'], // 自定义无衬线字体
-  CUSTOM_FONT_SERIF: ['LXGW WenKai'], // 自定义衬线字体
+  // CUSTOM_FONT_URL: ['https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css'], // 自定义字体的CSS
+  // CUSTOM_FONT_SANS: ['LXGW WenKai'], // 自定义无衬线字体
+  // CUSTOM_FONT_SERIF: ['LXGW WenKai'], // 自定义衬线字体
+
+  // CUSTOM_FONT_URL: ['https://fonts.googleapis.com/css2?family=Aref+Ruqaa&display=swap'], // URL to Google Fonts CSS
+  // CUSTOM_FONT_SANS: ['Aref Ruqaa'], // Specify the font name for sans-serif
+  // CUSTOM_FONT_SERIF: ['Aref Ruqaa'], // Specify the font name for serif
+  CUSTOM_FONT_URL: ['https://raw.githubusercontent.com/jasoncysiu/aino/refs/heads/main/aino-style.css'], // URL to Google Fonts CSS
+  CUSTOM_FONT_SANS: ['Aino'], // Specify the font name for sans-serif
+  CUSTOM_FONT_SERIF: ['Aino'], // Specify the font name for serif
 
   // 图标库CDN(可以直接改版本号）
   FONT_AWESOME_PATH: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/all.min.css',
